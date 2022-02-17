00a09368d9d4c605d926d0758bcce36ea69d73ea
David Pochet <david.pochet@gmail.com>
2022-02-17 18:48:40 +0100

    In work: loadme plugin

--- src/nmk_base/builders.py
+++ src/nmk_base/builders.py
@@ -1,5 +1,8 @@
 import shutil
 from pathlib import Path
+from typing import Dict
+
+from jinja2 import Template
 
 from nmk.model.builder import NmkTaskBuilder
 
@@ -15,3 +18,15 @@ class CleanBuilder(NmkTaskBuilder):
         else:
             # Nothing to clean
             self.logger.debug(f"Nothing to clean (folder not found: {to_delete})")
+
+
+class BuildLoadMe(NmkTaskBuilder):
+    def build(self, deps: Dict[str, str], venv_python: str):
+        # Iterate on outputs
+        for template, output in zip(self.task.inputs, self.task.outputs):
+            # Load template
+            self.logger.debug(f"Generating {output} from template {template}")
+            with template.open() as f, output.open("w") as o:
+                # Render it
+                t = Template(f.read())
+                o.write(t.render({"linuxVenvPython": venv_python}))
---
+++ templates/loadme.sh
@@ -0,0 +1,22 @@
+# 
+# !!! Generated file, don't edit !!!
+#
+
+# Create venv if not done yet
+if test ! -d venv; then
+    # Create it
+    {{ linuxVenvPython }} -m venv venv
+
+    # Load it
+    source venv/bin/activate
+    
+    # Bootstrap it
+    pip install pip wheel --upgrade
+
+    # Patch it
+    echo ' ' >> venv/bin/activate
+    echo 'eval $(register-python-argcomplete nmk)' >> venv/bin/activate
+fi
+
+# Just load it
+source venv/bin/activate
