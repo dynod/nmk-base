4235feb522f1fd544e006caf2c04fae2e51080f6
David Pochet <david.pochet@gmail.com>
2022-02-20 12:23:31 +0100

    Some more basic tasks -- Resolves dynod/nmk#41
    Added tasks:
    - version
    - help
    - tasks

--- src/nmk_base/builders.py
+++ src/nmk_base/builders.py
@@ -3,7 +3,9 @@ from pathlib import Path
 from typing import Dict, List
 
 from jinja2 import Template
+from rich.emoji import Emoji
 
+from nmk import __version__
 from nmk.model.builder import NmkTaskBuilder
 
 
@@ -30,3 +32,28 @@ class BuildLoadMe(NmkTaskBuilder):
                 # Render it
                 t = Template(f.read())
                 o.write(t.render({"pythonForVenv": venv_python}))
+
+
+class VersionBuilder(NmkTaskBuilder):
+    def build(self, plugins: Dict[str, str]):
+        # Displays all versions
+        all_versions = {"nmk": __version__}
+        all_versions.update(plugins)
+        for name, version in all_versions.items():
+            self.logger.info(self.task.emoji, f" {Emoji('backhand_index_pointing_right')} {name}: {version}")
+
+
+class HelpBuilder(NmkTaskBuilder):
+    def build(self, links: Dict[str, str]):
+        # Displays all online help links
+        all_links = {"nmk": "https://github.com/dynod/nmk/wiki"}
+        all_links.update(links)
+        for name, link in all_links.items():
+            self.logger.info(self.task.emoji, f" {Emoji('backhand_index_pointing_right')} {name}: {link}")
+
+
+class TaskListBuilder(NmkTaskBuilder):
+    def build(self):
+        # Iterate on all model tasks
+        for name, task in self.model.tasks.items():
+            self.logger.info(task.emoji, f" {Emoji('backhand_index_pointing_right')} {name}: {task.description}")
