3e0022dc38e79b3cc823295d12c1405324d93ed4
David Pochet <david.pochet@gmail.com>
2024-01-17 07:58:44 +0000

    Trigger buildenv init in skip mode

--- src/nmk_base/buildenv.py
+++ src/nmk_base/buildenv.py
@@ -63,14 +63,14 @@ class BuildenvInitBuilder(NmkTaskBuilder):
 
     def build(self, force: bool):
         """
-        Triggers BuildEnvManager init, in order to refresh buildenv loadinf scripts
+        Triggers BuildEnvManager init, in order to refresh buildenv loading scripts
         """
 
         # Prepare manager
         m = BuildEnvManager(self.model.config[NmkRootConfig.PROJECT_DIR].value, self._venv_bin_path())
 
-        # Trigger init
-        m.init(Namespace(force=force))
+        # Trigger init, either in force mode (typically for tests) or in skip mode (nominal case)
+        m.init(Namespace(force=force) if force else Namespace(skip=True))
 
         # Touch output files
         for f in self.outputs:
